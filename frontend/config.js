8797c2bdc27b1564680645182fefb9490bccf79b
Ankit <ankitnishad12@yahoo.com>
2026-09-26 15:21:28 +0530

    configured frontend with server

--- frontend/config.js
+++ frontend/config.js
@@ -1,5 +1,7 @@
+
+const apiUrl = import.meta.env.VITE_API_URL;
 const config = {
-    apiBaseUrl: 'http://localhost:8080/api/v1/shorten', // Replace with your actual API base URL
+    apiBaseUrl:     `${apiUrl}/api/v1/shorten`, // Replace with your actual API base URL
     apiKey: 'YOUR_API_KEY_HERE', // Replace with your actual API key if required
     defaultBackgroundImage: 'assets/bg5.jpg', // Default background image path
     
